6082a4c7dcd4a9589210a4a8c7ae6102169d5f39
Ka Du <notkrd@gmail.com>
2024-07-19 23:29:40 -0400

    Dockerfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const port = 3000
-const nav_port = 3000
+const nav_port = 9090
 const io = new Server(server)
 
 app.set('view engine', 'pug')
